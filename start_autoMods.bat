3559c42a1e023dae403e648875ce3385cd2a45a7
Andrew-InTheBox <andrew.box.81@gmail.com>
2025-12-25 19:08:46 -0600

    wipe config and new files setup

--- start_autoMods.bat
+++ start_autoMods.bat
@@ -1,7 +1,7 @@
 @echo off
 :start
 ::Name for the CMD window
-set serverName=ACBlocal
+set serverName=KRONJON-DeerIsle-Bots-PvE
 ::Server files location
 set serverLocation="C:\Program Files (x86)\Steam\steamapps\common\DayZServerDITrader"
 ::Server Port
@@ -36,10 +36,10 @@ echo Server mod list: %mods%
 echo (%time%) %serverName% started.
 
 ::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netLog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
-start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -mod=%mods% "-serverMod=_@DayZ-Dynamic-AI-Addon;_@Heatmap" -port=%serverPort% -profiles=%serverProfile% -cpuCount=%serverCPU% -adminlog -netlog -freezecheck 
+start "DayZ Server" /min "DayZServer_x64.exe" -config=%serverConfig% -mod=%mods% "-serverMod=_@Heatmap" -port=%serverPort% -profiles=%serverProfile% -cpuCount=%serverCPU% -adminlog -netlog -freezecheck 
 
 ::Time in seconds before kill server process (14400 = 4 hours)
-timeout 14390
+timeout 28000
 taskkill /im DayZServer_x64.exe /F
 ::Time in seconds to wait before..
 timeout 10
